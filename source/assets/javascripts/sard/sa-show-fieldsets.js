05035abe8ad7867d997aee872a961781ea28b691
Simon Sharville <design@simonsharville.co.uk>
2017-03-21 14:49:25 +0000

    Job Plan Timetable - Added Show/Hide function to modal

--- source/assets/javascripts/sard/sa-show-fieldsets.js
+++ source/assets/javascripts/sard/sa-show-fieldsets.js
@@ -1,13 +1,27 @@
+
+
+// Show/Hide Scheduled details =========
 // http://stackoverflow.com/questions/13689385/making-one-of-a-number-of-fieldsets-display-depending-on-a-selected-radio-button
 // http://jsfiddle.net/davidThomas/U2MKh/1/
 
-
-$('#scheduled_true').show();
-$('input:radio[name="type"]').click(function() {
-    var id = $(this).attr('data-targets'); // or: $(this).data('targets');
-    $('div [id="scheduled_true"]').hide();
-    $('#' + id).show();
+$(document).ready(function(){
+  $('#scheduled_true').show();
+  $('input:radio[name="type"]').click(function() {
+      var id = $(this).attr('data-targets'); // or: $(this).data('targets');
+      $('div [id="scheduled_true"]').hide();
+      $('#' + id).show();
+  });
 });
 
 
+// Show/Hide Repeats =========
+// http://stackoverflow.com/questions/2975521/show-hide-div-based-on-select-option-jquery
+// http://jsfiddle.net/FvMYz/
+
+$(document).ready(function(){
+        $('#repeatselector').change(function(){
+            $('.repeats').hide();
+            $('#' + $(this).val()).show();
+        });
+});
 
