89fd5d5c25828268a045f75c3da33a87561b1546
Simon Sharville <design@simonsharville.co.uk>
2017-03-21 15:25:00 +0000

    Javascript Cleanup
    Moved, Deleted or change some names of javascript files.

--- source/assets/javascripts/sard/sa-show-fieldsets.js
+++ source/assets/javascripts/sard/sa-show-fieldsets.js
@@ -1,6 +1,7 @@
 
 
 // Show/Hide Scheduled details =========
+// Used on Job Planing
 // http://stackoverflow.com/questions/13689385/making-one-of-a-number-of-fieldsets-display-depending-on-a-selected-radio-button
 // http://jsfiddle.net/davidThomas/U2MKh/1/
 
@@ -15,6 +16,7 @@ $(document).ready(function(){
 
 
 // Show/Hide Repeats =========
+// Used on Job Planing
 // http://stackoverflow.com/questions/2975521/show-hide-div-based-on-select-option-jquery
 // http://jsfiddle.net/FvMYz/
 
@@ -25,3 +27,36 @@ $(document).ready(function(){
         });
 });
 
+
+// Require if Active ========= 
+// Used on Appraisals / Personal Details
+// https://css-tricks.com/exposing-form-fields-radio-button-css/
+$(document).ready(function(){
+      var FormStuff = {
+      
+      init: function() {
+        this.applyConditionalRequired();
+        this.bindUIActions();
+      },
+      
+      bindUIActions: function() {
+        $("input[type='radio'], input[type='checkbox']").on("change", this.applyConditionalRequired);
+      },
+      
+      applyConditionalRequired: function() {
+        
+        $(".require-if-active").each(function() {
+          var el = $(this);
+          if ($(el.data("require-pair")).is(":checked")) {
+            el.prop("required", true);
+          } else {
+            el.prop("required", false);
+          }
+        });
+        
+      }
+      
+    };
+
+    FormStuff.init();
+});
